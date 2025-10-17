34d9e3dec1bf4db0d121486fa60dc633c200389d
Kasireddy Saiteja Reddy <kasireddysaitejareddy@gmail.com>
2025-10-17 20:58:42 +0530

    newfiles

--- ChoosingTeams.java
+++ ChoosingTeams.java
@@ -6,11 +6,11 @@ public class ChoosingTeams {
         Scanner sc=new Scanner(System.in);
         int n=sc.nextInt();
         int k=sc.nextInt();
-        int ans=0;
+        int ans=0,j=0;
         int arr[]=new int[n];
         for(int i=0;i<n;i++)
         {
-            arr[i]=sc.nextInt();
+            arr[j]=sc.nextInt();
         }
         Arrays.sort(arr);
         for(int i=0;i<arr.length;i+=3)
